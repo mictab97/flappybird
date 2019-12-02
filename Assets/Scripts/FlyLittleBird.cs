d6772a53aba25f92399152d59972f1092ee27164
mictab97 <57810604+mictab97@users.noreply.github.com>
2019-12-03 00:53:41 +0100

    update bird rotation and startscreen

---
+++ Assets/Scripts/FlyLittleBird.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyLittleBird : MonoBehaviour
+{
+    public GameManager gameManager;
+    public float velocity = 1;
+    private Rigidbody2D rb;
+
+    private int _score;
+    [SerializeField] private UIManager _uiManager;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) 
+        {
+        rb.velocity = Vector2.up * velocity;
+        } 
+    }
+
+
+    private void OnCollisioEnter2D(Collider2D collision)
+    {
+        gameManager.GameOver();
+    }
+    public void AddScore()
+    {
+        _score += 1;
+        _uiManager.SetScoreText(_score);
+    }
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        AddScore();
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _scoreText;
+
+    public void SetScoreText(int score)
+    {
+        _scoreText.SetText(score.ToString());
+    }
+}
